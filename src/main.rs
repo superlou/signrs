649cc442f3bbeaf776831b6e5640aaa10d95db95
superlou <lousimons@gmail.com>
2023-04-06 20:53:25 -0400

    Added image drawing to fix #6

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use std::time::Instant;
 use std::rc::Rc;
 use std::cell::RefCell;
 use std::path::{Path, PathBuf};
+use std::collections::HashMap;
 
 use speedy2d::Window;
+use speedy2d::image::{ImageHandle, ImageSmoothingMode};
 use speedy2d::shape::Rectangle;
 use speedy2d::window::{WindowHandler, WindowHelper};
 use speedy2d::Graphics2D;
@@ -19,12 +21,15 @@ struct SignWindowHandler {
     last_frame_time: Instant,
     graphics_calls: Rc<RefCell<Vec<GraphicsCalls>>>,
     root_path: Rc<RefCell<PathBuf>>,
+    image_handles: Rc<RefCell<HashMap<String, ImageHandle>>>,
 }
 
 enum GraphicsCalls {
     ClearScreen(Color),
     DrawRectangle(Rectangle, Color),
     DrawText(Vec2, Color, Rc<FormattedTextBlock>),
+    DrawImage(Vec2, String),
+    DrawRectangleImage(Rectangle, String),
 }
 
 impl WindowHandler for SignWindowHandler {
@@ -43,6 +48,44 @@ impl WindowHandler for SignWindowHandler {
                 GraphicsCalls::ClearScreen(c) => graphics.clear_screen(*c),
                 GraphicsCalls::DrawRectangle(r, c) => graphics.draw_rectangle(r.clone(), *c),
                 GraphicsCalls::DrawText(pos, c, block) => graphics.draw_text(pos, *c, block),
+                GraphicsCalls::DrawImage(pos, path_string) => {
+                    let mut created = false;
+                    let image_handle = match self.image_handles.borrow_mut().get_mut(path_string) {
+                        Some(image_handle) => image_handle.clone(),
+                        None => {
+                            let mut path = self.root_path.borrow().clone();
+                            path.push(path_string);
+                            let image_handle = graphics.create_image_from_file_path(None, ImageSmoothingMode::Linear, path).unwrap();
+                            created = true;
+                            image_handle
+                        }
+                    };
+                    
+                    if created {
+                        self.image_handles.borrow_mut().insert(path_string.to_owned(), image_handle.clone());
+                    }
+                    
+                    graphics.draw_image(pos, &image_handle);
+                },
+                GraphicsCalls::DrawRectangleImage(r, path_string) => {
+                    let mut created = false;
+                    let image_handle = match self.image_handles.borrow_mut().get_mut(path_string) {
+                        Some(image_handle) => image_handle.clone(),
+                        None => {
+                            let mut path = self.root_path.borrow().clone();
+                            path.push(path_string);
+                            let image_handle = graphics.create_image_from_file_path(None, ImageSmoothingMode::Linear, path).unwrap();
+                            created = true;
+                            image_handle
+                        }
+                    };
+                    
+                    if created {
+                        self.image_handles.borrow_mut().insert(path_string.to_owned(), image_handle.clone());
+                    }
+                    
+                    graphics.draw_rectangle_image(r.clone(), &image_handle);                    
+                },
             }
         }
         self.graphics_calls.borrow_mut().clear();
@@ -64,7 +107,7 @@ impl SignWindowHandler {
            graphics_calls.borrow_mut().push(GraphicsCalls::DrawRectangle(r, c));
         });
         
-        let graphics_calls = self.graphics_calls.clone();        
+        let graphics_calls = self.graphics_calls.clone();
         self.engine.register_fn("draw_text", move |text: &str, font: Font, scale: f32, color: Color, x: f32, y: f32| {
             let block = font.layout_text(text, scale, TextOptions::new());
             graphics_calls.borrow_mut().push(GraphicsCalls::DrawText((x, y).into(), color, block));
@@ -77,10 +120,9 @@ impl SignWindowHandler {
             .register_fn("new_color_from_rgba", Color::from_rgba);
         
         let root_path = self.root_path.clone();
-        
         self.engine.register_type_with_name::<Font>("Font")
             .register_fn("new_font", move |font_path: &str| {
-                let mut full_path = root_path.borrow_mut();
+                let mut full_path = root_path.borrow().clone();
                 full_path.push(font_path);
                 dbg!(&full_path);
                 
@@ -88,7 +130,24 @@ impl SignWindowHandler {
                 let font = Font::new(&bytes).unwrap();
                 font
         });
-                        
+       
+        self.engine.register_fn("new_image", move |path_string: &str| {
+            path_string.to_owned()
+        });
+        
+        let graphics_calls = self.graphics_calls.clone();
+        self.engine.register_fn("draw_image", move |path_string: &str, x: f32, y: f32| {
+            graphics_calls.borrow_mut().push(GraphicsCalls::DrawImage((x, y).into(), path_string.to_owned()));
+        });
+
+        let graphics_calls = self.graphics_calls.clone();
+        self.engine.register_fn("draw_image", move |path_string: &str, x: f32, y: f32, w: f32, h: f32| {
+            graphics_calls.borrow_mut().push(GraphicsCalls::DrawRectangleImage(
+                Rectangle::new((x, y).into(), (x + w, y + h).into()),
+                path_string.to_owned()
+            ));
+        });
+        
         let result = self.engine.eval_ast_with_scope::<()>(&mut self.scope, &self.ast);
         if let Err(err) = result {
             dbg!(&err);
@@ -124,6 +183,7 @@ fn main() {
         last_frame_time: Instant::now(),
         graphics_calls: Rc::new(RefCell::new(vec![])),
         root_path: Rc::new(RefCell::new(sign_root.to_path_buf())),
+        image_handles: Rc::new(RefCell::new(HashMap::new()))
     };
     
     handler.setup_engine();
