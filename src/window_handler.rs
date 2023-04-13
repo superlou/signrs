1d798bf687502c3cf4fc2ea015b3b4a0dd37e35c
superlou <lousimons@gmail.com>
2023-04-12 20:58:02 -0400

    Added very brittle watching of JSON files to fix #5

--- src/window_handler.rs
+++ src/window_handler.rs
@@ -3,7 +3,12 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::path::{Path, PathBuf};
+use std::fs;
+use std::sync::mpsc;
+use std::fs::read_to_string;
 
+use notify::{Watcher, RecursiveMode};
+use rhai::{Engine, Scope, AST, Array, CallFnOptions, FnPtr};
 use speedy2d::image::{ImageHandle, ImageSmoothingMode};
 use speedy2d::shape::Rectangle;
 use speedy2d::window::{WindowHandler, WindowHelper};
@@ -11,7 +16,6 @@ use speedy2d::Graphics2D;
 use speedy2d::color::Color;
 use speedy2d::font::{Font, TextLayout, TextOptions, FormattedTextBlock};
 use speedy2d::dimen::Vec2;
-use rhai::{Engine, Scope, AST, Array, CallFnOptions};
 
 pub struct SignWindowHandler {
     engine: Engine,
@@ -21,6 +25,12 @@ pub struct SignWindowHandler {
     graphics_calls: Rc<RefCell<Vec<GraphicsCalls>>>,
     root_path: Rc<RefCell<PathBuf>>,
     image_handles: Rc<RefCell<HashMap<String, ImageHandle>>>,
+    watches: Rc<RefCell<HashMap<PathBuf, FnPtr>>>,
+    
+    #[allow(dead_code)] // Required to keep watcher in scope
+    watcher: Box<dyn Watcher>,
+    
+    file_change_rx: mpsc::Receiver<PathBuf>,
 }
 
 enum GraphicsCalls {
@@ -36,12 +46,23 @@ impl WindowHandler for SignWindowHandler {
         let dt = self.last_frame_time.elapsed().as_secs_f32();
         self.last_frame_time = Instant::now();
       
+        // Check for changed files       
+        for changed_path_buf in self.file_change_rx.try_iter() {
+            if let Some(fn_ptr) = self.watches.borrow().get(&changed_path_buf) {
+                let json_text = read_to_string(&changed_path_buf).unwrap();
+                let json_data = self.engine.parse_json(json_text, true).unwrap();
+                fn_ptr.call::<()>(&self.engine, &self.ast, (json_data,)).unwrap();
+            }
+        }
+
+        // Call script draw function
         let options = CallFnOptions::new().eval_ast(false);
         let result = self.engine.call_fn_with_options::<()>(options, &mut self.scope, &mut self.ast, "draw", (dt,));
         if let Err(err) = result {
             dbg!(&err);
         }
 
+        // Perform queued graphic calls
         for call in self.graphics_calls.clone().borrow().iter() {
             match call {
                 GraphicsCalls::ClearScreen(c) => graphics.clear_screen(*c),
@@ -69,22 +90,44 @@ impl SignWindowHandler {
         
         let mut main_script = PathBuf::from(sign_root.as_ref());
         main_script.push("main.rhai");
-        let ast = engine.compile_file(main_script).unwrap();        
+        let ast = engine.compile_file(main_script).unwrap();
         
+        let (tx, rx) = mpsc::channel();
+        let tx_for_engine = tx.clone();
+         
+        let mut watcher = notify::recommended_watcher(move |res: Result<notify::Event, notify::Error>| {
+            match res {
+                Ok(event) => match event.kind {
+                    notify::EventKind::Modify(_) => {
+                      for path_buf in event.paths {
+                          let _ = tx.send(path_buf);
+                      }
+                    },
+                    _ => (),
+                },
+                Err(err) => println!("Watch error: {:?}", err),
+            }
+        }).unwrap();
+         
+        watcher.watch(sign_root.as_ref(), RecursiveMode::Recursive).unwrap();
+
         let mut handler = SignWindowHandler {
             engine, ast,
             scope: Scope::new(),
             last_frame_time: Instant::now(),
             graphics_calls: Rc::new(RefCell::new(vec![])),
             root_path: Rc::new(RefCell::new(sign_root.as_ref().to_path_buf())),
-            image_handles: Rc::new(RefCell::new(HashMap::new()))
+            image_handles: Rc::new(RefCell::new(HashMap::new())),
+            watches: Rc::new(RefCell::new(HashMap::new())),
+            watcher: Box::new(watcher),
+            file_change_rx: rx,
         };
         
-        handler.setup_engine();
+        handler.setup_engine(tx_for_engine);
         handler
     }
 
-    fn setup_engine(&mut self) {
+    fn setup_engine(&mut self, file_changed_tx: mpsc::Sender<PathBuf>) {
         let graphics_calls = self.graphics_calls.clone();       
         self.engine.register_fn("clear_screen", move |c: Color| {
            graphics_calls.borrow_mut().push(GraphicsCalls::ClearScreen(c));
@@ -145,7 +188,17 @@ impl SignWindowHandler {
                 path_string.to_owned(),
                 Color::from_rgba(1.0, 1.0, 1.0, alpha),
             ));
-        });        
+        });
+        
+        let root_path = self.root_path.clone();
+        let watches = self.watches.clone();
+        self.engine.register_fn("watch_json", move |path_string: &str, fn_ptr: FnPtr| {
+            let mut json_path = root_path.borrow().clone();
+            json_path.push(path_string);
+            let canonical_path = fs::canonicalize(json_path).unwrap();
+            watches.borrow_mut().insert(canonical_path.clone(), fn_ptr.clone());
+            file_changed_tx.send(canonical_path).unwrap();
+        });
         
         let result = self.engine.eval_ast_with_scope::<()>(&mut self.scope, &self.ast);
         if let Err(err) = result {
