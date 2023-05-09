df5e047030006cf77124e6db2e1d2524e2ddb183
superlou <lousimons@gmail.com>
2023-05-08 20:34:00 -0400

    Added Ember skeleton

--- src/server.rs
+++ src/server.rs
@@ -19,9 +19,14 @@ pub fn start_server(handler: &SignWindowHandler, sender: Mutex<UserEventSender<S
     let is_fullscreen = handler.is_fullscreen.clone();
     
     thread::spawn(move || {          
-        rouille::start_server("127.0.0.1:3000", move |request| {
+        rouille::start_server("127.0.0.1:3000", move |request| {         
+            let response = rouille::match_assets(&request, "frontend/dist");
+            if response.is_success() {
+                return response;
+            }
+            
             router!(request, 
-                (GET) (/status) => {
+                (GET) (/api/status) => {
                     let data = StatusResponse {
                       root_path: path.lock().unwrap().to_owned(),
                       is_fullscreen: *is_fullscreen.lock().unwrap(),
@@ -29,7 +34,7 @@ pub fn start_server(handler: &SignWindowHandler, sender: Mutex<UserEventSender<S
                     
                     Response::json(&data)
                 },
-                (GET) (/test_sender) => {
+                (GET) (/api/test_sender) => {
                     sender.lock().unwrap().send_event("Test".to_owned()).unwrap();
                     Response::text("Sending test")
                 },
